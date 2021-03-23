0c3397f781fcc47ed2cbe0e06d62e83daaddddce
chenfz <13277424603@163.com>
2021-03-23 11:00:35 +0800

    add: first file

---
+++ src/main/java/functionalprogramming/FunctionDemo.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2015-2021, 航天广信有限公司
+ * FileName: FunctionDemo
+ * Author:   chenfz
+ * Date:     2021/3/23 10:59
+ * Description: Function接口的使用
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package functionalprogramming;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈Function接口的使用〉
+ *
+ * @author chenfz
+ * @create 2021/3/23
+ * @since 1.0.0
+ */
+public class FunctionDemo {
+    public static void main(String[] args) {
+
+    }
+
+}
