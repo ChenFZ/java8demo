4bb8dfa69809b011b826fc0b5acf87b9494d0ac5
chenfz <13277424603@163.com>
2021-04-21 10:05:52 +0800

    add: Kafka module

--- src/main/java/functionalprogramming/FunctionDemo.java
+++ src/main/java/functionalprogramming/FunctionDemo.java
@@ -10,6 +10,10 @@
  */
 package functionalprogramming;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * 〈一句话功能简述〉<br> 
  * 〈Function接口的使用〉
@@ -20,7 +24,44 @@ package functionalprogramming;
  */
 public class FunctionDemo {
     public static void main(String[] args) {
+        //test1();
+        //test2();
+        test3();
+    }
 
+    private static void test3() {
+        int[] arr = {1,2,3,4};
+        System.out.println(arr.indexOf());
+    }
+
+    private static void test2() {
+        Set<Object> hashSet = new HashSet<>();
+        hashSet.addAll(Arrays.asList("Event","EventLifeCycle","Comment"));
+
+        Arrays.asList("Event","EventLifeCycle","Comment").stream().forEach(System.out::println);
+        String[] strs = {"Event","EventLifeCycle","Comment"};
+        Set<String> set = Stream.of("Event", "EventLifeCycle", "Comment").collect(Collectors.toSet());
+        System.out.println(set.contains("Event"));
+        System.out.println(set.add("Event"));
+        Map<String, String> map = set.stream().collect(Collectors.toMap(a -> a, a -> a));
+        map.forEach((key, value)-> System.out.println(key+"--"+value));
+
+        set.forEach(str-> System.out.println(str));
+    }
+
+    private static void test1() {
+        Map<String, String> map = new HashMap();
+        map.put("1", "xiaoming");
+        map.put("2", "hongxiang");
+        if(map.containsKey("1")){
+            map.remove("1");
+            System.out.println("contains");
+        }else{
+            map.remove("1");
+            System.out.println("don't contains");
+        }
+        System.out.println(map.get("1"));
+        map.forEach((key, value)-> System.out.println(key+"--"+value));
     }
 
 }
